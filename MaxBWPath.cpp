6e9bfeac513b6d0abd4da001c0e4584e24223971
akshaysarvesh25 <akshaysarvesh25@gmail.com>
2019-04-23 12:19:43 -0500

    Added the vector subroutines to take care of the Heap Functionality in the MaxBWPath.h file

---
+++ MaxBWPath.cpp
@@ -0,0 +1,250 @@
+/**********************************
+* Dijkstra's Algorihm
+* Maximum Bandwidth path
+*
+**********************************/
+#include<MaxBWPath.h>
+
+using namespace std;
+
+int main()
+{
+  /* Heap to store the graph data */
+  Heap<int> *Heap1 = new Heap<int>();
+  Heap<int> *Heap2 = new Heap<int>();
+
+  Heap<int> *Heap_test = new Heap<int>();
+
+  VecArray<int> *VecArray1 = new VecArray<int>();
+
+  /* Creating graphs of 5000 nodes as an adjacency list */
+  LinkedList<int> *Graph_1 = new LinkedList<int>[NUMBER_OF_VERTICES];
+  LinkedList<int> *Graph_2 = new LinkedList<int>[NUMBER_OF_VERTICES];
+
+  LinkedList<int> *Graph_test = new LinkedList<int>[5];
+  Graph_test[0].insert(0,99999);
+  Graph_test[0].insert(1,20);
+  Graph_test[0].insert(2,70);
+
+
+  Graph_test[1].insert(1,99999);
+  Graph_test[1].insert(3,3);
+  Graph_test[1].insert(0,20);
+
+  Graph_test[2].insert(2,99999);
+  Graph_test[2].insert(0,70);
+  Graph_test[2].insert(3,100);
+
+  Graph_test[3].insert(3,99999);
+  Graph_test[3].insert(1,3);
+  Graph_test[3].insert(2,100);
+
+
+
+  for(unsigned int i = 0;i<=3;i++)
+  {
+  cout<<i<<"->";
+  Graph_test[i].display();
+  }
+
+  srand(42);
+  /* Construct a cycle of connected nodes */
+  for(unsigned int i = 0;i<NUMBER_OF_VERTICES;i++)
+  {
+    unsigned int weight_insert = rand()%100;
+    if(i==(NUMBER_OF_VERTICES-1))
+    {
+      Graph_1[i].insert(i,99999);
+      Graph_1[i].insert(0,weight_insert);
+      continue;
+    }
+    Graph_1[i].insert(i,99999);
+    Graph_1[i].insert(i+1,weight_insert);
+  }
+
+  for(unsigned int i = 0;i<NUMBER_OF_VERTICES;i++)
+  {
+    for(unsigned int NV = 0;NV<(NUMBER_OF_VERTICES);NV++)
+    {
+      if((rand()%5000)<=6)
+      {
+        unsigned int weight_insert = rand()%100;
+        Graph_1[i].insert(NV,weight_insert);
+        Graph_1[NV].insert(i,weight_insert);
+      }
+
+    }
+
+  }
+
+  /*
+  Graph_test[0].ViewNodeStatus();
+  Graph_test[1].ViewNodeStatus();
+  Graph_test[2].ViewNodeStatus();
+  */
+
+  /* Make all nodes of the graph unseen */
+  for(unsigned int count = 0;count<NUMBER_OF_VERTICES;count++)
+  {
+    Graph_1[count].MakeNodesUnseen();
+    //cout<<"count is : "<<count<<endl;
+  }
+
+  /*
+  Graph_test[0].ViewNodeStatus();
+  Graph_test[1].ViewNodeStatus();
+  Graph_test[2].ViewNodeStatus();
+  */
+
+  Graph_1[0].MakeNodesIntree();
+
+  Graph_1[0].SetNodeBandwidth(99999);
+
+  int Node_prev = 0;
+
+  std::vector<int> AdjacentNodes = Graph_1[0].GetAdjacentNodes();
+  std::vector<int> AdjacentNodeWeights = Graph_1[0].GetAdjacentNodesWeights();
+  /*
+  if(Heap_test->SizeOfHeapIsZero() == 1)
+  {
+    cout<<"Heap is null"<<endl;
+    cout<<Heap_test->SizeOfHeapIsZero()<<endl;
+  }*/
+
+  /*
+  Graph_test[0].ViewNodeStatus();
+  Graph_test[1].ViewNodeStatus();
+  Graph_test[2].ViewNodeStatus();
+  */
+  for(unsigned int count = 1;count<=(AdjacentNodes.size()-1);count++)
+  {
+
+    int AdjNode = (int)AdjacentNodes[count];
+    #if OUTPUT_LOG
+    cout<<"Nodename : "<<AdjNode<<endl;
+    cout<<"Count : "<<count<<endl;
+    #endif
+    int AdjNodeWeight = (int)AdjacentNodeWeights[count];
+    Graph_1[AdjNode].MakeNodesFringe();
+    Graph_1[AdjNode].SetDad(0);
+    Graph_1[AdjNode].GetDad();
+    Graph_1[AdjNode].SetNodeBandwidth(AdjNodeWeight);
+    bool b = Heap_test->Insert(AdjNode,AdjNodeWeight);
+
+  }
+
+  cout<<"\n__First stage done__\n"<<endl;
+
+  /* Printing the heap data structure */
+  Heap_test->print();
+  cout<<"\n\nStarting the algorithm.. \n"<<"_____"<<endl;
+
+  while(Heap_test->SizeOfHeapIsZero()!=0)
+  {
+    int Node = Heap_test->MaxEdge();
+    int weight = Heap_test->MaxWeight();
+    #if OUTPUT_LOG
+    cout<<"Node and weight : "<<Node<<" "<<weight<<endl;
+    #endif
+    //cout<<Heap_test->SizeOfHeapIsZero()<<endl;
+    if(Heap_test->position[Node]!=-1)
+    {
+        Heap_test->Delete(Heap_test->position[Node]);
+    }
+
+    #if OUTPUT_LOG
+    Heap_test->print();
+    #endif
+    Graph_1[Node].MakeNodesIntree();
+    //cout<<Heap_test->SizeOfHeapIsZero()<<endl;
+    //std::vector<int> AdjacentNodes = Graph_test[Node].GetAdjacentNodesWithout(Node_prev);
+    std::vector<int> AdjacentNodes = Graph_1[Node].GetAdjacentNodes();//Without(Node_prev);
+    std::vector<int> AdjacentNodeWeights = Graph_1[Node].GetAdjacentNodesWeights();//Without(Node_prev);
+    Node_prev = Node;
+
+    for(unsigned int count = 1;count<AdjacentNodes.size();count++)
+    {
+      int AdjNode = (int)AdjacentNodes[count];
+      //cout<<"Status of the node "<<AdjNode<<" : "<<endl;
+      Graph_1[AdjNode].ViewNodeStatus();
+
+      int AdjNodeBW1 = Graph_1[AdjNode].GetBandWidth();
+      int BW_node1 = Graph_1[Node].GetBandWidth();
+      int AdjNode_Weight1 = Graph_1[Node].GetWeightOfAdjNode(AdjNode);/* TODO */
+
+
+      #if OUTPUT_LOG
+      cout<<"BW_Adj Node, BW_Node, AdjNode_Weight "<<AdjNodeBW1<<" , "<<BW_node1<<" , "<<AdjNode_Weight1<<endl;
+      #endif
+
+      if(Graph_1[AdjNode].IsNodeUnseen() == true)
+      {
+        #if OUTPUT_LOG
+        cout << "\033[1;32mNode unseen  \033[0m"<<AdjNode<<std::endl;
+        #endif
+        Graph_1[AdjNode].MakeNodesFringe();
+        Graph_1[AdjNode].SetDad(Node);
+        //int BW_node = Graph_test[Node].GetBandWidth();
+        //int AdjNode_Weight = Graph_test[AdjNode].GetWeight();
+        #if OUTPUT_LOG
+        cout<<"Weight of the adjacent node and bandwidth of the current node : "<<AdjNode_Weight1<<" & "<<BW_node1<<endl;
+        #endif
+        Graph_1[AdjNode].SetNodeBandwidth(std::min({BW_node1,AdjNode_Weight1}));
+        int AdjNodeBW = Graph_1[AdjNode].GetBandWidth();
+        Heap_test->Insert(AdjNode,AdjNodeBW);
+        Heap_test->print();
+      }
+
+      else if((Graph_1[AdjNode].IsNodeFringe() == true)&&(AdjNodeBW1<(std::min({BW_node1,AdjNode_Weight1}))))
+      {
+        #if OUTPUT_LOG
+        cout << "\033[1;32mNode is fringe : \033[0m"<<AdjNode<<std::endl;
+        #endif
+        if(Heap_test->position[AdjNode]!=-1)
+        {
+          Heap_test->Delete(Heap_test->position[AdjNode]);
+        }
+
+        Heap_test->print();
+        Graph_1[AdjNode].SetDad(Node);
+        //ToDo
+        Graph_1[AdjNode].SetNodeBandwidth(std::min({BW_node1,AdjNode_Weight1}));
+        int AdjNodeBW2 = Graph_1[AdjNode].GetBandWidth();
+        Heap_test->Insert(AdjNode,AdjNodeBW2);
+        Heap_test->print();
+
+      }
+
+      else
+      {
+        #if OUTPUT_LOG
+        std::cout << "\033[1;31mNot necessary to process Element of heap : \033[0m"<<AdjNode<<std::endl;
+        #endif
+      }
+
+    }
+    #if OUTPUT_LOG
+    cout<<"____"<<endl;
+    #endif
+  }
+
+  int Node_D = 999;
+  cout<<Node_D<<"<-";
+  while(Graph_1[Node_D].DisplayDadNodes()!=0)
+  {
+    cout<<Graph_1[Node_D].DisplayDadNodes()<<"<-";
+    Node_D=Graph_1[Node_D].DisplayDadNodes();
+  }
+  cout<<"0"<<endl;
+
+
+
+
+
+
+
+
+
+
+  return 0;
+}
---
+++ MaxBWPath.h
@@ -0,0 +1,563 @@
+#include<stdlib.h>
+#include <iostream>
+#include<vector>
+#include <cmath>
+#include <math.h>
+#include <string.h>
+#include <time.h>
+#include <random>
+#include<algorithm>
+
+using namespace std;
+
+#define OUTPUT_LOG 0
+
+
+template <class T>
+class Heap{
+
+  private:
+    std::vector<T> H;
+    std::vector<T> EdgeWeight;
+    T *tail;
+
+    std::vector<T> Swap(std::vector<T> H_,int x,int y)
+    {
+      T temp = H_[x];
+      H_[x] = H_[y];
+      H_[y] = temp;
+
+
+      return H_;
+    }
+
+    bool MakeHeap_Parent(std::vector<T> weight_MP, std::vector<T> H_,int n)
+    {
+      //cout<<"Index : "<<" : "<<(n-1)/2<<endl;
+      unsigned int Parent_idx = (double)(n-1)/2;
+      //cout<<"Parent_idx : "<<Parent_idx<<endl;
+      if(weight_MP[n]<=weight_MP[Parent_idx])
+      {
+        return true;
+
+      }
+
+      else
+      {
+        std::vector<T> H1 = Swap(H_,n,Parent_idx);
+        H = H1;
+        int TempPos = position[H_[n]];
+        position[H_[n]] = position[H_[Parent_idx]];
+        position[H_[Parent_idx]] = TempPos;
+        std::vector<T> weight_MP_1 = Swap(weight_MP,n,Parent_idx);
+        EdgeWeight = weight_MP_1;
+        return MakeHeap_Parent(weight_MP_1,H1,Parent_idx);
+      }
+
+    }
+
+    bool MakeHeap_Child(std::vector<T> weight11,std::vector<T> H11,int i)
+    {
+      if((2*i)+1>(H11.size()-1))
+      {
+        return true;
+      }
+
+      if(weight11[((2*i)+1)]>=weight11[((2*i)+2)])
+      {
+        if(weight11[i]>=weight11[((2*i)+2)])
+        {
+          return true;
+        }
+        else
+        {
+          H = Swap(H11,i,((2*i)+1));
+          int TempPos = position[H[i]];
+          position[H[i]] = position[H[(2*i)+1]];
+          position[H[(2*i)+1]] = TempPos;
+          EdgeWeight = Swap(weight11,i,((2*i)+1));
+          return MakeHeap_Child(EdgeWeight,H,((2*i)+1));
+        }
+      }
+      else
+      {
+        if(weight11[i]>=weight11[((2*i)+2)])
+        {
+          return true;
+        }
+
+        else
+        {
+          H = Swap(H11,i,((2*i)+2));
+          int TempPos = position[H[i]];
+          position[H[i]] = position[H[(2*i)+1]];
+          position[H[(2*i)+1]] = TempPos;
+          EdgeWeight = Swap(weight11,i,((2*i)+2));
+          return MakeHeap_Child(EdgeWeight,H,((2*i)+2));
+        }
+      }
+    }
+
+  public:
+    /* Position of the element in the heap */
+    int position[5000];
+
+    bool Insert(T value,T weight)
+    {
+      if(H.size() == 0)
+      {
+        H.push_back(value);
+        EdgeWeight.push_back(weight);
+        position[value] = H.size()-1;
+        //cout<<"Added an element, H.size() is : "<<H.size()<<endl;
+        return true;
+      }
+
+      else
+      {
+        //cout<<"size : "<<H.size()<<endl;
+        H.push_back(value);
+        EdgeWeight.push_back(weight);
+
+        position[value] = H.size()-1;
+        //cout<<"Adding element to heap"<<endl;
+        //cout<<"H.size() : "<<H.size()<<endl;
+        return MakeHeap_Parent(EdgeWeight,H,(H.size()-1));
+
+      }
+    }
+
+    void print()
+    {
+      #if OUTPUT_LOG
+      std::cout << "\033[1;33mContents of the heap : \033[0m"<<std::endl;
+      for(unsigned int i = 0;i<H.size();i++)
+      {
+        std::cout<<H[i]<<" "<<EdgeWeight[i]<<std::endl;
+      }
+      std::cout << "\033[1;33mEnd of heap\033[0m"<<std::endl;
+      #endif
+    }
+
+    T MaxEdge()
+    {
+      return H[0];
+    }
+
+    T MaxWeight()
+    {
+      return EdgeWeight[0];
+    }
+
+
+    bool Delete(T index)
+    {
+      if(H.size() == 0)
+      {
+        cout<<"Nothing to delete"<<endl;
+        return false;
+      }
+
+      else if(H.size()==1)
+      {
+        cout<<"Deleting the last element in the tree.."<<endl;
+        H.erase(H.end()-1);
+        EdgeWeight.erase(EdgeWeight.end()-1);
+        return true;
+
+      }
+
+      else
+      {
+        int TempPos1 = position[H[index]];
+        position[H[index]] = -1;
+        H[index] = H[H.size()-1];
+        position[H[H.size()-1]] = TempPos1;
+        EdgeWeight[index] = EdgeWeight[EdgeWeight.size()-1];
+        H.erase(H.end()-1);
+        EdgeWeight.erase(EdgeWeight.end()-1);
+        unsigned int P_idx = (double)(index-1)/2;
+
+        if(EdgeWeight[index]>=EdgeWeight[P_idx])
+        {
+          return MakeHeap_Parent(EdgeWeight,H,index);
+        }
+        else if((EdgeWeight[index]<=EdgeWeight[(2*index)+1])||(EdgeWeight[index]<=EdgeWeight[(2*index)+2]))
+        {
+          return MakeHeap_Child(EdgeWeight,H,index);
+        }
+      }
+
+    }
+
+    int SizeOfHeapIsZero()
+    {
+      return H.size();
+    }
+
+
+
+};
+
+
+template<class T>
+class VecArray
+{
+private:
+  std::vector<T> NodeName;
+  std::vector<T> NodeWeight;
+
+  void print(const std::vector<int> &vec)
+  {
+    for (const auto& i: vec)
+    std::cout << i << ' ';
+    std::cout << std::endl;
+  }
+
+public:
+  bool Insert_VecArray(T val,T wt)
+  {
+    NodeName.push_back(val);
+    NodeWeight.push_back(wt);
+  }
+
+  bool Delete_VecArray(T val)
+  {
+    /* Find the position of val in the array */
+    std::vector<int>::iterator it = std::find(NodeName.begin(), NodeName.end(), val);
+    int index = std::distance(NodeName.begin(), it);
+    /* Erase the Node from NodeName */
+    NodeName.erase( std::remove( NodeName.begin(), NodeName.end(), val ), NodeName.end() );
+    /* Erase the Node from Nodeweight */
+    NodeWeight.erase( std::remove( NodeWeight.begin(), NodeWeight.end(), NodeWeight[index] ), NodeWeight.end() );
+  }
+
+  T MaxEdge()
+  {
+    std::vector<int>::iterator result = std::max_element(NodeWeight.begin(),NodeWeight.end());
+    int index = std::distance(NodeWeight.begin(), result);
+    return NodeName[index];
+  }
+
+  T MaxWeight()
+  {
+    std::vector<int>::iterator result = std::max_element(NodeWeight.begin(),NodeWeight.end());
+    return *result;
+  }
+
+  void PrintArrays()
+  {
+    #if OUTPUT_LOG
+    std::cout << "\033[1;33mContents of the array : \033[0m"<<std::endl;
+    print(NodeName);
+    print(NodeWeight);
+    std::cout << "\033[1;33mEnd of heap\033[0m"<<std::endl;
+    #endif
+  }
+
+};
+
+#define NUMBER_OF_VERTICES 5000
+#define NUMBER_OF_EDGES_PER_VERTEX_AVG 6
+#define PERCENTAGE_VERTICES_NEIGHBORS 1000
+
+#define INSERT_DEBUG_OUTPUT 0
+#define DISPLAY_DEBUG_OUTPUT 1
+
+using namespace std;
+
+typedef enum
+{
+  STATUS_NONE = 0,
+  STATUS_GRAPH_UNSEEN = 1,
+  STATUS_GRAPH_FRINGE = 2,
+  STATUS_GRAPH_INTREE = 3,
+}NODE_STATUS;
+
+
+typedef struct
+{
+  unsigned int NodeName;
+  unsigned int NodeWeight;
+}NodeAttributes;
+
+template <class T>
+struct SingleLinkedListNode{
+  T data;
+  T weight;
+  NODE_STATUS NodeStatus;
+  T Bandwidth;
+  T dad;
+  SingleLinkedListNode *NextNode;
+
+  SingleLinkedListNode(){
+
+
+  }
+
+  SingleLinkedListNode(T val){
+
+    this->data = val;
+  }
+
+  SingleLinkedListNode(T val,SingleLinkedListNode<T> NewNode){
+
+    this->data = val;
+    this->NextNode = NewNode;
+  }
+
+
+};
+
+
+
+
+template <class T>
+class LinkedList{
+
+  private:
+    SingleLinkedListNode<T> *root, *tail;
+
+  public:
+    void insert(T val,T EdgeWeight)
+    {
+
+      SingleLinkedListNode<T> *tempNode=new SingleLinkedListNode<T>;
+      tempNode->data=val;
+      tempNode->weight = EdgeWeight;
+      tempNode->NodeStatus = STATUS_NONE;
+      tempNode->NextNode=NULL;
+
+      if(root)
+      {
+        #if INSERT_DEBUG_OUTPUT
+        cout<<"Adding new elements "<<endl;
+        #endif
+        tail->NextNode=tempNode;
+        tail=tempNode;
+      }
+      else
+      {
+        root=tempNode;
+        tail=tempNode;
+        tempNode=NULL;
+        #if INSERT_DEBUG_OUTPUT
+        cout<<"New root added : "<<val<<endl;
+        #endif
+      }
+
+    }
+
+    void display()
+    {
+      SingleLinkedListNode<T> *tempNode=new SingleLinkedListNode<T>;
+      tempNode=this->root;
+      unsigned int i=0;
+      //cout<<"\n"<<endl;
+      while(tempNode!=NULL)
+      {
+        cout<<tempNode->data<<" -> ";
+        tempNode=tempNode->NextNode;
+      }
+      cout<<"\n";
+    }
+
+    void MakeNodesUnseen()
+    {
+      SingleLinkedListNode<T> *tempNode=new SingleLinkedListNode<T>;
+      tempNode=this->root;
+      unsigned int i=0;
+      //cout<<"\n"<<endl;
+        //cout<<tempNode->data<<" -> ";
+      tempNode->NodeStatus = STATUS_GRAPH_UNSEEN;
+      tempNode=tempNode->NextNode;
+
+    }
+
+    void MakeNodesIntree()
+    {
+      SingleLinkedListNode<T> *tempNode=new SingleLinkedListNode<T>;
+      tempNode=this->root;
+      unsigned int i=0;
+      tempNode->NodeStatus = STATUS_GRAPH_INTREE;
+    }
+
+    void MakeNodesFringe()
+    {
+      SingleLinkedListNode<T> *tempNode=new SingleLinkedListNode<T>;
+      tempNode=this->root;
+      unsigned int i=0;
+      tempNode->NodeStatus = STATUS_GRAPH_FRINGE;
+    }
+
+    void SetNodeBandwidth(T BWvalue)
+    {
+      SingleLinkedListNode<T> *tempNode=new SingleLinkedListNode<T>;
+      tempNode=this->root;
+
+      //cout<<tempNode->data<<" -> ";
+      tempNode->Bandwidth = BWvalue;
+
+    }
+
+    std::vector<int> GetAdjacentNodes()
+    {
+      std::vector<int> temp;
+      SingleLinkedListNode<T> *tempNode=new SingleLinkedListNode<T>;
+      tempNode=this->root;
+      #if OUTPUT_LOG
+      cout<<"Adjacent nodes : "<<endl;
+      #endif
+      while(tempNode!=NULL)
+      {
+        temp.push_back(tempNode->data);
+        #if OUTPUT_LOG
+        cout<<tempNode->data<<endl;
+        #endif
+        tempNode=tempNode->NextNode;
+      }
+
+      return temp;
+    }
+
+    std::vector<int> GetAdjacentNodesWithout(int NodeName)
+    {
+      std::vector<int> temp;
+      SingleLinkedListNode<T> *tempNode=new SingleLinkedListNode<T>;
+      tempNode=this->root;
+      #if OUTPUT_LOG
+      cout<<"Adjacent nodes : "<<endl;
+      #endif
+      while(tempNode!=NULL)
+      {
+        if(tempNode->data!=NodeName)
+        {
+          temp.push_back(tempNode->data);
+          cout<<tempNode->data<<endl;
+        }
+
+        tempNode=tempNode->NextNode;
+      }
+
+      return temp;
+    }
+
+    std::vector<int> GetAdjacentNodesWeights()
+    {
+      std::vector<int> temp;
+      SingleLinkedListNode<T> *tempNode=new SingleLinkedListNode<T>;
+      tempNode=this->root;
+
+      while(tempNode!=NULL)
+      {
+        temp.push_back(tempNode->weight);
+        tempNode=tempNode->NextNode;
+      }
+
+      return temp;
+    }
+
+    void SetDad(int dadval)
+    {
+      SingleLinkedListNode<T> *tempNode=new SingleLinkedListNode<T>;
+      tempNode=this->root;
+      tempNode->dad = dadval;
+    }
+
+    void GetDad()
+    {
+      SingleLinkedListNode<T> *tempNode=new SingleLinkedListNode<T>;
+      tempNode=this->root;
+      #if OUTPUT_LOG
+      cout<<"dad of : "<<tempNode->data<<" is : "<<tempNode->dad<<endl;
+      #endif
+    }
+
+    void ViewNodeStatus()
+    {
+      SingleLinkedListNode<T> *tempNode=new SingleLinkedListNode<T>;
+      tempNode=this->root;
+      #if OUTPUT_LOG
+      cout<<"Node status of : "<<tempNode->data<<" is : "<<tempNode->NodeStatus<<"\n";
+      cout<<"Node Bandwidth of : "<<tempNode->data<<" is : "<<tempNode->Bandwidth<<"\n";
+      #endif
+    }
+
+    bool IsNodeUnseen()
+    {
+      SingleLinkedListNode<T> *tempNode=new SingleLinkedListNode<T>;
+      tempNode=this->root;
+
+      if(tempNode->NodeStatus == STATUS_GRAPH_UNSEEN)
+      {
+        return true;
+      }
+
+      else
+      {
+        return false;
+      }
+
+    }
+
+
+    bool IsNodeFringe()
+    {
+      SingleLinkedListNode<T> *tempNode=new SingleLinkedListNode<T>;
+      tempNode=this->root;
+
+      if(tempNode->NodeStatus == STATUS_GRAPH_FRINGE)
+      {
+        return true;
+      }
+
+      else
+      {
+        return false;
+      }
+
+    }
+
+    T GetBandWidth()
+    {
+      SingleLinkedListNode<T> *tempNode=new SingleLinkedListNode<T>;
+      tempNode=this->root;
+      return tempNode->Bandwidth;
+    }
+
+    T GetWeightOfAdjNode(T val)
+    {
+      SingleLinkedListNode<T> *tempNode=new SingleLinkedListNode<T>;
+      tempNode=this->root;
+
+      while((tempNode!=NULL))
+      {
+        if(tempNode->data == val)
+        {
+          return tempNode->weight;
+        }
+        tempNode=tempNode->NextNode;
+      }
+
+      return 99999;
+    }
+
+    T GetWeight()
+    {
+      SingleLinkedListNode<T> *tempNode=new SingleLinkedListNode<T>;
+      tempNode=this->root;
+      return tempNode->weight;
+    }
+
+    T DisplayDadNodes()
+    {
+      SingleLinkedListNode<T> *tempNode=new SingleLinkedListNode<T>;
+      tempNode=this->root;
+
+      //cout<<tempNode->dad<<" -> ";
+      //tempNode=tempNode->NextNode;
+
+      return tempNode->dad;
+    }
+
+};
